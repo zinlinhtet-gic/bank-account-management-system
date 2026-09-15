10de6b9b908f634bbad99826bcd9d4677025ce36
zinlinhtet-gic <zinlinhtet@gicjp.com>
2026-09-15 10:51:32 +0630

    initial commit

---
+++ bams.server/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace bams.server.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+{
+}
